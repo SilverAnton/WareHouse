1f1068f8dbc32c40252481a59e46bada96d9ea34
serebriakovanton <cu-hawk@yandex.ru>
2024-10-11 18:14:02 +0700

    add models: Product, Order, OrderItem. add schemas: product, order, оrder_tem. add routers with CRUD: r_product, r_order create async Session in config/db.py, full integration with PostgreSQL create main.py with FastAPI app and include model routers

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from routers import r_product, r_order
-
-app = FastAPI()
-
-app.include_router(r_product.router, prefix='/products', tags=['Products'])
-app.include_router(r_order.router, prefix='/orders', tags=['Orders'])
-
-
--- schemas/order_item.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class OrderItemCreate(BaseModel):
-    product_id: int
-    quantity: int = 1  # Устанавливаем значение по умолчанию на 1
-
-    class Config:
-        from_attributes = True
